864e21623b58d2e7a7febf941fa4aa03cc5ab387
Jaicke Santos <jaicke-santos@outlook.com>
2020-04-29 18:35:49 -0300

    Fix: response from get numbers endpoint

--- app/server.rb
+++ app/server.rb
@@ -7,6 +7,6 @@ before do
 end
 
 get '/get_numbers' do
-  return Constants::DATABASES[params[:db].to_sym].get_random_numbers.to_json if params[:db] && Constants::POSSIBLE_DBS.include?(params[:db])
+  return { numbers: Constants::DATABASES[params[:db].to_sym].get_random_numbers }.to_json if params[:db] && Constants::POSSIBLE_DBS.include?(params[:db])
   halt 404, { error: "Invalid parameter db. Possible values: #{Constants::POSSIBLE_DBS}."}.to_json
 end
